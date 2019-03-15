95b99867fe783862305a1c5dbfbf010406f7145a
Jeff Muizelaar <jrmuizel@gmail.com>
2019-03-15 11:02:41 -0400

    CpuAll

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::time::Instant;
+use num_cpus;
 
 fn mem_bench(count: u32) {
     let size = 3840*2160*4*count as usize;
@@ -13,35 +14,54 @@ fn mem_bench(count: u32) {
 }
 
 fn work_loop(count: i32) {
-    for i in 0..10000000 {
+    for i in 0..count {
         std::time::Instant::now();
     }
 }
 
 
-fn cpu_bench(count: u32) {
+fn cpu_bench(core: usize, count: u32) {
     loop {
         let begin = std::time::Instant::now();
         work_loop(10_i32.pow(count));
         let end = std::time::Instant::now();
-        println!("time: {} ms", (end - begin).as_nanos() as f32 / (60. * 1000. * 1000.));
+        println!("{}: time: {} ms", core, (end - begin).as_nanos() as f32 / (60. * 1000. * 1000.));
     }
 }
 
+enum Kind {
+    Cpu,
+    CpuAll,
+    Mem
+}
 
 fn main() {
     let opt = std::env::args().nth(1);
-    let cpu = if let Some(opt) = opt {
-        opt == "cpu"
+    let kind = if let Some(kind) = opt {
+        match kind.as_ref() {
+            "cpu" => Kind::Cpu,
+            "cpuall" => Kind::CpuAll,
+            _ => Kind::Mem
+        }
     } else {
-        false
+        Kind::Mem
     };
 
     let amount = std::env::args().nth(2).and_then(|x| x.parse().ok());
 
-    if cpu {
-        cpu_bench(amount.unwrap_or(6));
-    } else {
-        mem_bench(amount.unwrap_or(60));
+    match kind {
+        Kind::Cpu => cpu_bench(0, amount.unwrap_or(8)),
+        Kind::Mem => mem_bench(amount.unwrap_or(60)),
+        Kind::CpuAll => {
+            let count = num_cpus::get();
+            let mut join = None;
+            for i in 0..count {
+                join = Some(std::thread::spawn(move || {
+                    cpu_bench(i, amount.unwrap_or(8))
+                }));
+            }
+            join.unwrap().join();
+        },
+
     }
 }
