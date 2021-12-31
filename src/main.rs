80d19ef80ecc0377475b45f1d217d3c66ed1241b
Jeff Muizelaar <jrmuizel@gmail.com>
2021-12-31 16:58:20 -0500

    Add GB/s measurement

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,27 @@ fn mem_bench(count: u32) {
     let src: Vec<u8> = vec![1; size];
     let mut dst: Vec<u8> = vec![0; size];
     loop {
-        let begin = std::time::Instant::now();
+        let begin = Instant::now();
         dst.copy_from_slice(&src[..]);
-        let end = std::time::Instant::now();
-        println!("time: {} ms", (end - begin).as_nanos() as f32 / (count as f32 * 1000. * 1000.));
+        let end = Instant::now();
+        let elapased_nanos = (end - begin).as_nanos() as f32;
+        println!("time: {:.2} ms {:.2} GB/s",  elapased_nanos / (count as f32 * 1000. * 1000.), size  as f32 / elapased_nanos);
     }
 }
 
 fn work_loop(count: i32) {
     for i in 0..count {
-        std::time::Instant::now();
+        Instant::now();
     }
 }
 
 
 fn cpu_bench(core: usize, count: u32) {
     loop {
-        let begin = std::time::Instant::now();
+        let begin = Instant::now();
         work_loop(10_i32.pow(count));
-        let end = std::time::Instant::now();
-        println!("{}: time: {} ms", core, (end - begin).as_nanos() as f32 / (60. * 1000. * 1000.));
+        let end = Instant::now();
+        println!("{}: time: {:.2} ms", core, (end - begin).as_nanos() as f32 / (60. * 1000. * 1000.));
     }
 }
 
